ef093f8728dd6ee4111cd634f169f7639a1eb9d4
yingchiehchan <yingchiehchan@users.noreply.github.com>
2026-08-01 20:02:58 +0800

    Simplify home navigation labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'good-day-web-v4';
+const CACHE = 'good-day-web-v5';
 const LOCAL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(LOCAL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
